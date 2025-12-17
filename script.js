98098bbffa7a5455f4d3acb614645b9c2dffcfd4
Dammyy22 <adeosunoluwadamilare09@gmail.com>
2025-12-17 08:13:56 +0100

    Update homepage

--- script.js
+++ script.js
@@ -108,3 +108,10 @@ function changeHeroBackground() {
 
 // Change background every 5 seconds
 setInterval(changeHeroBackground, 5000);
+
+const menuBtn = document.querySelector('.menu-btn');
+const navLinks = document.querySelector('.nav-links');
+
+menuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
